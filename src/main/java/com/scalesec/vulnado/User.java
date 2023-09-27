a3f369f9de542b5e7f881ca7e22755455141bafb
charithlanzzz <lansakaracharith@gmail.com>
2023-09-27 15:35:04 +0530

    changes

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,13 +1,15 @@
 package com.scalesec.vulnado;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
+
+import static com.scalesec.vulnado.Postgres.connection;
 
 public class User {
   public String id, username, hashedPassword;
@@ -19,46 +21,55 @@ public class User {
   }
 
   public String token(String secret) {
-    SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+    byte[] secretBytes = secret.getBytes(StandardCharsets.UTF_8); // Specify encoding
+    SecretKey key = Keys.hmacShaKeyFor(secretBytes);
     String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
     return jws;
   }
 
+
   public static void assertAuth(String secret, String token) {
     try {
-      SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+      byte[] secretBytes = secret.getBytes(StandardCharsets.UTF_8); // Specify encoding
+      SecretKey key = Keys.hmacShaKeyFor(secretBytes);
       Jwts.parser()
-        .setSigningKey(key)
-        .parseClaimsJws(token);
-    } catch(Exception e) {
+              .setSigningKey(key)
+              .parseClaimsJws(token);
+    } catch (Exception e) {
       e.printStackTrace();
       throw new Unauthorized(e.getMessage());
     }
   }
 
+
   public static User fetch(String un) {
-    Statement stmt = null;
     User user = null;
-    try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
+
+    try (Connection cxn = connection()) {
       System.out.println("Opened database successfully");
 
-      String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
-      if (rs.next()) {
-        String user_id = rs.getString("user_id");
-        String username = rs.getString("username");
-        String password = rs.getString("password");
-        user = new User(user_id, username, password);
+      String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+
+      try (PreparedStatement preparedStatement = cxn.prepareStatement(query)) {
+        preparedStatement.setString(1, un);
+
+        ResultSet rs = preparedStatement.executeQuery();
+
+        if (rs.next()) {
+          String user_id = rs.getString("user_id");
+          String username = rs.getString("username");
+          String password = rs.getString("password");
+          user = new User(user_id, username, password);
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
       }
-      cxn.close();
-    } catch (Exception e) {
+    } catch (SQLException e) {
       e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return user;
     }
+
+    return user;
   }
 }
+
+
